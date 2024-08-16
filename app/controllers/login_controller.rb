47e33b4eefb93514d5f894401c2b4a79095c134e
FranciscoLN <franciscocolatinodelima@gmail.com>
2024-08-15 22:49:35 -0300

    style: remove trash code

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,12 +1,9 @@
 class LoginController < ApplicationController
   skip_before_action :authenticate_request, only: [:index, :login]
 
-  # GET /users or /users.json
   def index
   end
 
-
-  # GET /users/new
   def login
     service = Users::Authenticate.call(login_params)
 
@@ -16,7 +13,6 @@ class LoginController < ApplicationController
     else
       redirect_to login_path, status: :unauthorized, notice: "Usuário ou senha inválidos"
     end
-    #return render :login, notice: service.errors, status: :unauthorized unless service.success?
   end
 
   def logout
@@ -24,52 +20,9 @@ class LoginController < ApplicationController
     redirect_to login_path
   end
 
-  # GET /users/1/edit
-
-
-  # POST /users or /users.json
-  # def create
-  #   @user = User.new(user_params)
-
-  #   respond_to do |format|
-  #     if @user.save
-  #       @token = ::JsonWebToken.encode(user_id: @user.id)
-  #       format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-  #       format.json { render :show, status: :created, location: @user }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
-    # Only allow a list of trusted parameters through.
-    def login_params
-      params.permit(:username, :password_digest, :commit)
-    end
+  def login_params
+    params.permit(:username, :password_digest, :commit)
+  end
 end
