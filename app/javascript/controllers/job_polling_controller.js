3ad4636b6c3104ed989df8fe4f5f613cd4a146d9
FranciscoLN <franciscocolatinodelima@gmail.com>
2024-08-18 13:52:48 -0300

    feat: add link to invoice(show)

--- app/javascript/controllers/job_polling_controller.js
+++ app/javascript/controllers/job_polling_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = { jobId: String } 
-  static targets = ['status', 'progress', 'progressBar', 'errors'];
+  static targets = ['status', 'progress', 'progressBar', 'errors', 'linkToInvoice'];
 
   apiUrl = `/jobs/${this.jobIdValue}`
   headers = { 'Accept': 'application/json' }
@@ -24,13 +24,14 @@ export default class extends Controller {
         this.updateStatus(data.status);
         this.updateProgress(data.progress);
         this.updateErrors(data.errors);
-        this.checkStopCondition(data.status);
+        this.checkStopCondition(data);
       });
   }
 
-  checkStopCondition(status) {
-    if (status === 'done' || status === 'failure') {
+  checkStopCondition(data) {
+    if (data.status === 'done' || data.status === 'failure') {
       clearInterval(this.pollingIntervalId);
+      if (data.status === 'done') { this.updateLinkToInvoice(`/invoices/${data.content.invoice_id}`); }
     }
   }
 
@@ -41,9 +42,13 @@ export default class extends Controller {
   updateProgress(progress) {
     this.progressTarget.innerHTML = progress;
     this.progressBarTarget.style.width = `${progress}%`;
-  }
+  } 
 
   updateErrors(errors) {
     this.errorsTarget.innerHTML = errors?.join(', ') || 'Nenhum.';
+  }
+  updateLinkToInvoice(link) {
+    this.linkToInvoiceTarget.href = link;
+    this.linkToInvoiceTarget.setAttribute('class', 'btn btn-success');
   }
 }
